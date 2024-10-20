a2a728ba8ff76b38f01581f5f13fab45122422ef
Rcid <rcid@MacBook-Pro-de-Rcid.local>
2024-10-19 23:33:21 -0300

    correccion js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+
+///////////////////////// SLIDER /////////////////////////
+
 const track = document.querySelector('.slide-track');// Inicializa la posición del slide al hacer click en el botón prev
 const slides = document.querySelectorAll('.slide');// Inicializa la posición del slide al hacer click en el botón prev
 const prevButton = document.getElementById('prev');// Inicializa la posición del slide al hacer click en el botón prev
@@ -32,4 +35,59 @@ prevButton.addEventListener('click', () => {
 });
 
 
+///////////////////////// ANIMACIONES AL HACER SCROLL /////////////////////////
 
+// Selecciona todos los elementos que quieres animar al hacer scroll
+const elements = document.querySelectorAll('.animate-on-scroll');
+
+// Configura el Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Remueve las clases de animación antes de agregarla para forzar la animación
+      entry.target.classList.remove('animate__animated', 'animate__fadeInUp');
+
+      // Usar setTimeout para permitir que el navegador procese la remoción de clases y luego agregar la animación
+      setTimeout(() => {
+        entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+      }, 100); // 100ms de retraso para asegurar la transición fluida
+    } else {
+      // Remueve la clase cuando el elemento no es visible para reiniciar la animación
+      entry.target.classList.remove('animate__animated', 'animate__fadeInUp');
+    }
+  });
+}, {
+  threshold: 0.5 // El 50% del elemento debe estar visible para activar la animación
+});
+
+// Asocia el Intersection Observer con los elementos
+elements.forEach(element => {
+  observer.observe(element);
+});
+
+
+///////////////////////// MENU RESPONSIVE /////////////////////////
+
+
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+// Verificar si los elementos existen
+if (burger && mobileMenu) {
+    // Función para alternar el menú móvil
+    const toggleMobileMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+    };
+
+    // Agregar evento al botón burger
+    burger.addEventListener('click', toggleMobileMenu);
+
+    // Cerrar el menú al hacer clic en un enlace
+    mobileMenu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.add('hidden'); // Oculta el menú
+        });
+    });
+} else {
+    console.warn('No se encontraron los elementos burger o mobileMenu');
+}
